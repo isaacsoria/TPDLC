23003de583c7781b1609c69bd62993efe40baa21
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-04-24 16:04:14 -0300

    Agrego imagenes en la pai de spotify

--- Practico_1_APIs/js/api_spotify.js
+++ Practico_1_APIs/js/api_spotify.js
@@ -16,54 +16,48 @@
 				}
 				
 				$.get( "https://api.spotify.com/v1/search?q="+busqueda+"&type="+resultadoTipo, function(resultados) {
-					$("#contenedorResultadosSpotify > div *:not(:first-child)").remove();
+					$("#contenedorResultadosSpotify > div > div:not(:first-child)").remove();
 					console.log(resultados);
 					indiceColor=0;
 					if(resultados.artists!==undefined){
-						for(var i = 0; i<resultados.artists.items.length; i++){
-							var clone = $("#baseResultadoBusquedaSpotify").clone();
-							clone.find(".name a").html(resultados.artists.items[i].name);
-							clone.find(".name a").attr("href", resultados.artists.items[i].href);
-							//clone.find(".image img").attr("src", resultados.artists.items[i].images[0].url);
-							clone.css("background-color",colores[indiceColor]);
-							$("#contenedorResultadosSpotify > div").append(clone);
-						}
+						cargarDatos(resultados.artists,colores[indiceColor]);
 					}
 					indiceColor=1;
 					if(resultados.albums!==undefined){
-						for(var i = 0; i<resultados.albums.items.length; i++){
-							var clone = $("#baseResultadoBusquedaSpotify").clone();
-							clone.find(".name a").html(resultados.albums.items[i].name);
-							clone.find(".name a").attr("href", resultados.albums.items[i].href);
-							//clone.find(".image img").attr("src", resultados.albums.items[i].images[0].url);
-							clone.css("background-color",colores[indiceColor]);
-							$("#contenedorResultadosSpotify > div").append(clone);
-						}
+						cargarDatos(resultados.albums,colores[indiceColor]);
 					}
 					indiceColor=2;
 					if(resultados.tracks!==undefined){
-						for(var i = 0; i<resultados.tracks.items.length; i++){
-							var clone = $("#baseResultadoBusquedaSpotify").clone();
-							clone.find(".name a").html(resultados.tracks.items[i].name);
-							clone.find(".name a").attr("href", resultados.tracks.items[i].href);
-							//clone.find(".image img").attr("src", resultados.tracks.items[i].images[0].url);
-							clone.css("background-color",colores[indiceColor]);
-							$("#contenedorResultadosSpotify > div").append(clone);
-						}
+						cargarDatos(resultados.tracks,colores[indiceColor]);
 					}
 					indiceColor=3;
 					if(resultados.playlists!==undefined){
-						for(var i = 0; i<resultados.playlists.items.length; i++){
-							var clone = $("#baseResultadoBusquedaSpotify").clone();
-							clone.find(".name a").html(resultados.playlists.items[i].name);
-							clone.find(".name a").attr("href", resultados.playlists.items[i].href);
-							//clone.find(".image img").attr("src", resultados.playlists.items[i].images[0].url);
-							clone.css("background-color",colores[indiceColor]);
-							$("#contenedorResultadosSpotify > div").append(clone);
-						}
+						cargarDatos(resultados.playlists,colores[indiceColor]);
 					}
 				});
 			}
 		});
+		function cargarDatos(datos, color){
+			var tamanioMinimo = 200;
+			for(var i = 0; i<datos.items.length; i++){
+				var clone = $("#baseResultadoBusquedaSpotify").clone();
+				var item = datos.items[i];
+				clone.find(".name a").html(item.name);
+				clone.find(".name a").attr("href", item.external_urls.spotify);
+				var masChica = 100000, imagenMasChica = null;
+				for(var j=0; j < item.images.length; j++){
+					if(item.images[j].width < masChica && item.images[j].width > tamanioMinimo){
+						masChica = item.images[j].width;
+						imagenMasChica = item.images[j].url;
+					}
+				}
+				if(imagenMasChica !== null){
+					clone.find(".image img").attr("src", imagenMasChica);
+					clone.find(".image img").attr("width", "100px");
+				}
+				clone.css("background-color",color);
+				$("#contenedorResultadosSpotify > div").append(clone);
+			}
+		}
 	});
 })();
